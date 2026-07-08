54c4a14223c7823bac69a3d66496178ccd3cad54
Sam MG Harish <147686318+sam-mg@users.noreply.github.com>
2026-07-08 13:41:48 +0530

    Updating version for PYPI Release.

--- lu77U_MobileSec/__init__.py
+++ lu77U_MobileSec/__init__.py
@@ -1,6 +1,6 @@
 """lu77U-MobileSec - The Only Mobile Security Tool Which You Need"""
 
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 __author__ = "Sam MG Harish"
 __email__ = "sammgharish@gmail.com"
 
